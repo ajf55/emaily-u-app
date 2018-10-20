8efe7429a05667659dbc3d6b11b6c895df191462
Drew Fraser <drewfraser8@gmail.com>
2018-10-19 18:22:31 -0700

    commit2

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const app = express();
 
 // creating the route handlers
 app.get('/', (req, res)=> {
-  res.send({hi: 'there'})
+  res.send({ hi: 'there' });
 });
 
 
 // look at underlying environment and see if there is a port declared
-// for "|| 5000" it means that if there isn't a dynamically assigned port form cloud application, default 5000
+// for "|| 5000" it means that if there isn't a dynamically assigned port form cloud application, default 5000 ; eg dev environment
 const PORT = process.env.PORT || 5000;
 // labeling the port - manual test with http://localhost:5000/
-app.listen(5000);
+app.listen(PORT);
