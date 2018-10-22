350f4eed36a696402781657e2d25635304662a1e
Drew Fraser <drewfraser8@gmail.com>
2018-10-21 21:28:35 -0700

    proxy support for google strategy

--- services/passport.js
+++ services/passport.js
@@ -2,11 +2,11 @@ const passport = require("passport");
 // import the Google strategy and use the .Strategy param
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 // creating the app function for the generic route handlers
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
 // pull a model out of mongoose
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
   // done is a callback (if error, user.id)
@@ -14,37 +14,38 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser ((id, done) => {
+passport.deserializeUser((id, done) => {
   User.findById(id).then(user => {
-      done(null, user);
-    });
+    done(null, user);
+  });
 });
 
 // passport.use is a generic register
 // new GoogleStrategy creates a new instance Google Strategy
 
 passport.use(
-// https://console.developers.google.com
+  // https://console.developers.google.com
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       // check if user already exists
       // returns a promise
-      User.findOne({ googleID: profile.id }).then((existingUser) => {
-          if (existingUser) {
-            // we already have a record with the given profile ID
-            done(null, existingUser);
-          } else {
-            // we don't have a user record with this ID
-            new User ({ googleID: profile.id })
-              .save()
-              .then(user => done(null, user));
-            // .save takes the record and saves to the DB
-          }
+      User.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+          done(null, existingUser);
+        } else {
+          // we don't have a user record with this ID
+          new User({ googleID: profile.id })
+            .save()
+            .then(user => done(null, user));
+          // .save takes the record and saves to the DB
+        }
       });
     }
   )
