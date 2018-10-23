e70f44a8b62e7e19325ea373525d83a311d96036
Drew Fraser <drewfraser8@gmail.com>
2018-10-22 21:46:58 -0700

    updated location

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
           >
             Learn React
           </a>
-          <a href="http://localhost:5000/auth/google">Sign in with Google</a>
+          <a href="/auth/google">Sign in with Google</a>
         </header>
       </div>
     );
