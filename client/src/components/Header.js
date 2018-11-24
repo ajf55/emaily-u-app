3fab07694996e48dd3c9ff60abaae5b2ab7cb74c
Drew Fraser <drewfraser8@gmail.com>
2018-11-23 20:17:45 -0500

    commit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,25 @@
+// class based component (not a functional component)
 import React, { Component } from 'react';
 
 class Header extends Component {
-  
+  render(){
+    return (
+      <div>
+        <nav>
+          <div className = "nav-wrapper">
+            <a className = "left brand-logo">
+              Emaily
+            </a>
+              <ul className = "right">
+                <li>
+                  <a>Login With Google</a>
+                </li>
+              </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
+
+export default Header;
