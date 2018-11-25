a91643bc1cfd1efa76e2bbe6c3d3e619596e6968
Drew Fraser <drewfraser8@gmail.com>
2018-11-24 23:39:23 -0500

    updated link nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,40 @@
 // class based component (not a functional component)
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
-  render(){
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
+  render() {
     return (
       <div>
         <nav>
-          <div className = "nav-wrapper">
-            <a className = "left brand-logo">
+          <div className="nav-wrapper">
+            <Link
+              to={this.props.auth ? "/surveys" : "/"}
+              className="left brand-logo"
+            >
               Emaily
-            </a>
-              <ul className = "right">
-                <li>
-                  <a>Login with Google</a>
-                </li>
-              </ul>
+            </Link>
+            <ul className="right">{this.renderContent()}</ul>
           </div>
         </nav>
       </div>
@@ -22,4 +42,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
