4c687b7dd71166a04223fb1cb0dc0b6b0bdfd919
Drew Fraser <drewfraser8@gmail.com>
2019-02-10 15:14:06 -0800

    updated filenames

---
+++ models/Survey.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+// const Schema = mongoose.Schema; -> destructured
+const { Schema } = mongoose;
+
+// create Schema and add/remove properties (can freely do so later)
+// schema type or use an object
+const userSchema = new Schema({
+  googleId: String,
+  credits: { type: Number, default: 0 }
+});
+
+// create model class
+mongoose.model("users", userSchema);
