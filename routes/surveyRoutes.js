2d812129b10761f6c04dbf1a3a98291843677ea7
Drew Fraser <drewfraser8@gmail.com>
2019-02-10 01:55:28 -0800

    ts

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,5 @@
 const _ = require("lodash");
-const Path = require("path-parser").default;
-// url is a default node.js library
+const Path = require("path-parser");
 const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
@@ -10,39 +9,31 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 const Survey = mongoose.model("surveys");
 
-// survey route handler
 module.exports = app => {
   app.get("/api/surveys", requireLogin, async (req, res) => {
     const surveys = await Survey.find({ _user: req.user.id }).select({
       recipients: false
     });
+
     res.send(surveys);
   });
 
   app.get("/api/surveys/:surveyId/:choice", (req, res) => {
-    console.log(choice);
     res.send("Thanks for voting!");
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
-    const p = Path.createPath("/api/surveys/:surveyId/:choice");
+    const p = new Path("/api/surveys/:surveyId/:choice");
 
     _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname);
         if (match) {
-          return {
-            email,
-            surveyId: match.surveyId,
-            choice: match.choice
-          };
+          return { email, surveyId: match.surveyId, choice: match.choice };
         }
       })
-      // compact function takes an array and removes undefined
       .compact()
-      // remove duplicate records
       .uniqBy("email", "surveyId")
-      // at the end of lodash chain pull out the value
       .each(({ surveyId, email, choice }) => {
         Survey.updateOne(
           {
@@ -57,7 +48,6 @@ module.exports = app => {
             lastResponded: new Date()
           }
         ).exec();
-        // .exec() executes 'updateOne' in mongoose
       })
       .value();
 
@@ -65,7 +55,6 @@ module.exports = app => {
   });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
-    // post survey request should include neccesary props
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -76,16 +65,13 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
-    // send the email after Survey has been created
-    // when we use a class inside of js to create a new instance of a class use 'new' keyword
-    // pass in data arguments
+
+    // Great place to send an email!
     const mailer = new Mailer(survey, surveyTemplate(survey));
 
-    // try function to catch errors
     try {
       await mailer.send();
       await survey.save();
-      // payments
       req.user.credits -= 1;
       const user = await req.user.save();
 
