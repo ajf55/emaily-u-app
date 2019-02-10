d12c8d1105db7747b4c1d6758c8a58ba0b2f7386
Drew Fraser <drewfraser8@gmail.com>
2019-02-10 02:33:48 -0800

    ts

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// import express using syntax that works with node.js
-// when app first boots up this is the configuration that needs to load
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
@@ -10,45 +8,37 @@ require("./models/User");
 require("./models/Survey");
 require("./services/passport");
 
-mongoose.connect(
-  keys.mongoURI,
-  { useNewUrlParser: true }
-);
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-// middlewares are wired up to express via app.use before request handlers
 app.use(bodyParser.json());
-
 app.use(
   cookieSession({
-    // convert 30 days to milisecond
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    // encrypt the cookie
     keys: [keys.cookieKey]
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-// require and call functions
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 require("./routes/surveyRoutes")(app);
 
-// route handling for requests that need to go to React App or client index.html catch all
-if ((process.env.NODE_ENV = "production")) {
-  // express will serve production assets
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
   app.use(express.static("client/build"));
-  // express will servve index.html file if it doesn't recognize the route
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
   const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-// look at underlying environment and see if there is a port declared
-// for "|| 5000" it means that if there isn't a dynamically assigned port form cloud application, default 5000 ; eg dev environment
 const PORT = process.env.PORT || 5000;
-// labeling the port - manual test with http://localhost:5000/
 app.listen(PORT);
